fb9cc466882d4f7dd406b89b79c4c8698368104d
albaniahazera <alzgamer2005@gmail.com>
2025-09-09 16:06:05 +0700

    MCS-SETUP V-1.1.0

--- Start.java
+++ Start.java
@@ -16,110 +16,145 @@ import java.util.Scanner;
 
 public class Start{
     public static void main(String[] args) {
-        String PROGRAM_VERSION = "1.0.0";
+        String PROGRAM_VERSION = "1.1.0";
+        String raw_user_choose;
+        String user_choose;
         String minecraft_version;
         Runtime.Version runtime_version = Runtime.version();
         int major_version = runtime_version.feature();
 
         
-        System.out.println("Java version: " + runtime_version); 
-        System.out.println("Java major version: " + major_version);
+        System.out.println("[MCS-SETUP]: Java version: " + runtime_version); 
+        System.out.println("[MCS-SETUP]: Java major version: " + major_version);
 
         if (major_version >= 14) {
             ClearConsole();
-            System.out.println("=" .repeat(65));
+            System.out.println("=" .repeat(63));
             System.out.println("\t\t\tMCS-SETUP");
-            System.out.println("-" .repeat(65));
+            System.out.println("-" .repeat(63));
             System.out.println("Tools for setup minecraft server automation [BY Albania Hazera]");
-            System.out.println("-" .repeat(65));
+            System.out.println("-" .repeat(63));
             System.out.println("\t\t\tVersion: " + PROGRAM_VERSION);
-            System.out.println("=" .repeat(65));
-    
+            System.out.println("=" .repeat(63));
+            System.out.println("Supported Minecraft server versions:");
+            System.out.println("[1] 1.21.1");
+            System.out.println("[2] 1.21.2");
+
             try (Scanner scanner = new Scanner(System.in)) {
-                System.out.print("Enter Minecraft version: ");
-                minecraft_version = scanner.nextLine();
-                if (minecraft_version.equals(" ") || minecraft_version.isEmpty()) {
-                    System.out.println("Invalid input. Please enter a valid Minecraft version.");
-                    System.out.println("Program will exit.");
-                }else if (minecraft_version.equals("1.21.1")) {
-                    String url_eula = "https://aka.ms/MinecraftEULA";
-                    System.out.println("You choose minecraft server " + minecraft_version + ", For continue you must be accept eula: " + url_eula);
-                    System.out.print("agree/no: ");
-                    String user_agree = scanner.nextLine();
-                    switch (user_agree) {
-                        case "agree" -> SetupServer(minecraft_version);
-                        case "no" -> System.out.println("Program will exit. You must choose agree for continue the program.");
-                        default -> {
-                            System.out.println("Invalid input. Please choose agree/no");
-                            System.out.println("Program will exit.");
-                        }
-                    }
+                System.out.print("Choose 1 - 5: ");
+                raw_user_choose = scanner.nextLine();
+                user_choose = raw_user_choose.trim();
+                if (user_choose.equals(" ") || user_choose.isEmpty()) {
+                    System.out.println("[MCS-SETUP]: Invalid input. Please enter a valid Minecraft version.");
+                    System.out.println("[MCS-SETUP]: Program will exit.\n");
+                }else if (user_choose.equals("1")) {
+                    minecraft_version = "1.21.1";
+                    ChooseVersion(minecraft_version);
+                }else if (user_choose.equals("2")) {
+                    minecraft_version = "1.21.2";
+                    ChooseVersion(minecraft_version);
                 }else {
-                    System.out.println("Version " + minecraft_version + " is not supported yet.");
-                    System.out.println("Program will exit.");
+                    System.out.println("[MCS-SETUP]: Please choose a valid option.");
+                    System.out.println("[MCS-SETUP]: Program will exit.\n");
                 }
             }
         }else {
-            System.out.println("Unsupported Java version. You must install java 14 or above for running this program.");
+            System.out.println("[MCS-SETUP]: Unsupported Java version. You must install java 14 or above for running this program.");
             System.exit(1);
         }
     }
 
+    public static void ChooseVersion(String minecraft_version) {
+        String url_eula = "https://aka.ms/MinecraftEULA";
+        System.out.println("[MCS-SETUP]: You choose minecraft server " + minecraft_version + ", For continue you must be accept eula: " + url_eula);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("agree/no: ");
+            String user_agree = scanner.nextLine();
+            switch (user_agree) {
+                case "agree" -> SetupServer(minecraft_version);
+                case "no" -> System.out.println("[MCS-SETUP]: Program will exit. You must choose agree for continue the program.\n");
+                default -> {
+                    System.out.println("[MCS-SETUP]: Invalid input. Please input agree/no");
+                    System.out.println("[MCS-SETUP]: Program will exit.\n");
+                }
+            }
+        }
+    }
+
     public static void SetupServer(String minecraft_version) {
-        System.out.println("Setting up Minecraft server for version: " + minecraft_version);
+        System.out.println("[MCS-SETUP]: Setting up Minecraft server for version: " + minecraft_version);
         String version_folder_name = "Server" + File.separator + "version" + File.separator + minecraft_version;
         File version_folder = new File(version_folder_name);
 
         if (version_folder.mkdirs()) {
-            System.out.println("Created directory: " + version_folder.getAbsolutePath());
+            System.out.println("[MCS-SETUP]: Created directory: " + version_folder.getAbsolutePath());
             DownloadServerFile(minecraft_version, version_folder);
         }else {
-            System.out.println("Directory already exists.");
+            System.out.println("[MCS-SETUP]: Directory already exists.");
         }
     }
 
     public static void DownloadServerFile(String minecraft_version, File folder_name) {
-        System.out.println("Preparing download server files for version: " + minecraft_version);
+        System.out.println("[MCS-SETUP]: Preparing download server files for version: " + minecraft_version);
         String[] file_urls = {
             // 1.21.1
-            "https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar"
+            "https://piston-data.mojang.com/v1/objects/59353fb40c36d304f2035d51e7d6e6baa98dc05c/server.jar",
+            // 1.21.2
+            "https://piston-data.mojang.com/v1/objects/7bf95409b0d9b5388bfea3704ec92012d273c14c/server.jar"
         };
         String save_path = folder_name.getPath();
         String file_name = "server.jar";
 
-        try {
-            DownloadFile(file_urls[0], save_path + File.separator + file_name);
-            System.out.println("\nDownloaded server file successfully.");
-            RunFileServer(save_path, file_name);
-        }catch (IOException e) {
-            System.out.println("\nFailed to download server file: " + e.getMessage());
+        if (minecraft_version.equals("1.21.1")) {
+            System.out.println("[MCS-SETUP]: Downloading server file for version 1.21.1...");
+            try {
+                DownloadFile(file_urls[0], save_path + File.separator + file_name);
+                System.out.println("\n[MCS-SETUP]: Downloaded server file successfully.");
+                RunFileServer(save_path, file_name);
+            }catch (IOException e) {
+                System.out.println("\n[MCS-SETUP]: Failed to download server file: " + e.getMessage());
+            }
+        }else if (minecraft_version.equals("1.21.2")) {
+            System.out.println("[MCS-SETUP]: Downloading server file for version 1.21.2...");
+            try {
+                DownloadFile(file_urls[1], save_path + File.separator + file_name);
+                System.out.println("\n[MCS-SETUP]: Downloaded server file successfully.");
+                RunFileServer(save_path, file_name);
+            }catch (IOException e) {
+                System.out.println("\n[MCS-SETUP]: Failed to download server file: " + e.getMessage());
+            }
         }
+        
     }
 
     public static void RunFileServer(String save_path, String file_name) {
         File server_dir = new File(save_path);
         if (server_dir.mkdirs()) {
-            System.out.println("Created server directory...");
+            System.out.println("[MCS-SETUP]: Created server directory...");
         }else {
-            System.out.println("Directory already exist.");
+            System.out.println("[MCS-SETUP]: Directory already exist.");
         }
 
         String jar_path = new File(server_dir, file_name).getAbsolutePath();
-        System.out.println("Preparing execute file server...");
+        System.out.println("[MCS-SETUP]: Preparing execute file server...");
 
         try {
             ProcessBuilder pb = new ProcessBuilder("java", "-jar", jar_path, "nogui");
             pb.directory(server_dir);
             pb.inheritIO();
             Process process = pb.start();
-            System.out.println("Execute jar file...");
+            System.out.println("[MCS-SETUP]: Execute jar file...");
+            System.out.println("\n\t\t\tMINECRAFT OUTPUT, PLEASE WAIT...");
+            System.out.println("=".repeat(63) + "\n");
             int exit_code = process.waitFor();
-            System.out.println("Execute file finished. With exit code: " + exit_code);
+            System.out.println("\n" + ("=".repeat(63)));
+            System.out.println("\t\t\tMINECRAFT OUTPUT END\n");
+            System.out.println("[MCS-SETUP]: Execute file finished. With exit code: " + exit_code);
             RunServer(server_dir, save_path);
         }catch (IOException e) {
-            System.out.println("Failed execute jar file: " + e.getMessage());
+            System.out.println("[MCS-SETUP]: Failed execute jar file: " + e.getMessage());
         }catch (InterruptedException e) {
-            System.out.println("Process was interrupted: " + e.getMessage());
+            System.out.println("[MCS-SETUP]: Process was interrupted: " + e.getMessage());
             Thread.currentThread().interrupt();
         }
     }
@@ -134,7 +169,7 @@ public class Start{
                 lines.add(line);
             }
         }catch (IOException e) {
-            System.out.println("Failed read file eula.txt: " + e.getMessage());
+            System.out.println("[MCS-SETUP]: Failed read file eula.txt: " + e.getMessage());
             return;
         }
 
@@ -149,10 +184,10 @@ public class Start{
                 writer.write(line);
                 writer.newLine();
             }
-            System.out.println("EULA has set to true by program.");
+            System.out.println("[MCS-SETUP]: EULA has set to true by program.");
             CheckOS(server_path.getAbsolutePath());
         } catch (IOException e) {
-            System.out.println("Failed writen file eula.txt: " + e.getMessage());
+            System.out.println("[MCS-SETUP]: Failed writen file eula.txt: " + e.getMessage());
         }
     }
 
@@ -160,13 +195,13 @@ public class Start{
 
         String os = System.getProperty("os.name").toLowerCase();
         if (os.contains("win")) {
-            System.out.println("Preparing created batch file for Windows...");
+            System.out.println("[MCS-SETUP]: Preparing created batch file for Windows...");
             CreatedBatchFile(save_path);
         }else if (os.contains("nix") || (os.contains("nux")) || (os.contains("mac"))) {
-            System.out.println("Preparing created shell file for Linux/Mac...");
+            System.out.println("[MCS-SETUP]: Preparing created shell file for Linux/Mac...");
             CreatedShellFile(save_path);
         }else {
-            System.out.println("Unsupported operating system. This program supports Windows, Linux, and Mac.");
+            System.out.println("[MCS-SETUP]: Unsupported operating system. This program supports Windows, Linux, and Mac.");
             System.exit(1);
         }
     }
@@ -183,12 +218,12 @@ public class Start{
 
         try (FileWriter writer = new FileWriter(file_path)) {
             writer.write(content);
-            System.out.println("File [" + file_path + "] created successfully.");
-            System.out.println("To start the server, run the following command: ");
-            System.out.println("cd " + run_dir);
-            System.out.println("Type: " + ".\\start_server.bat" + " and enter");
+            System.out.println("[MCS-SETUP]: File [" + file_path + "] created successfully.");
+            System.out.println("[MCS-SETUP]: To start the server, run the following command: ");
+            System.out.println("[MCS-SETUP]: cd " + run_dir);
+            System.out.println("[MCS-SETUP]: Type -> [.\\start_server.bat] and enter\n");
         }catch (IOException e) {
-            System.out.println("Failed create file [" + file_path + "]" + e.getMessage());
+            System.out.println("[MCS-SETUP]: Failed create file [" + file_path + "]" + e.getMessage());
         }
     }
 
@@ -203,23 +238,23 @@ public class Start{
 
         try (FileWriter writer = new FileWriter(file_path)) {
             writer.write(content);
-            System.out.println("File [" + file_path + "] created successfully.");
+            System.out.println("[MCS-SETUP]: File [" + file_path + "] created successfully.");
             // Make the shell script executable
             ProcessBuilder pb = new ProcessBuilder("chmod", "+x", file_path);
             Process process = pb.start();
             try {
                 process.waitFor();
-                System.out.println("File [" + file_path + "] has been made executable.");
-                System.out.println("To start the server, run the following command: ");
-                System.out.println("cd " + run_dir);
-                System.out.println("Type: " + "./start_server.sh" + " and enter");
+                System.out.println("[MCS-SETUP]: File [" + file_path + "] has been made executable.");
+                System.out.println("[MCS-SETUP]: To start the server, run the following command: ");
+                System.out.println("[MCS-SETUP]: cd " + run_dir);
+                System.out.println("[MCS-SETUP]: Type -> [./start_server.sh] and enter\n");
             } catch (InterruptedException e) {
-                System.out.println("The process was interrupted: " + e.getMessage());
+                System.out.println("[MCS-SETUP]: The process was interrupted: " + e.getMessage());
                 Thread.currentThread().interrupt();
             }
 
         }catch (IOException e) {
-            System.out.println("Failed create file [" + file_path + "]" + e.getMessage());
+            System.out.println("[MCS-SETUP]: Failed create file [" + file_path + "]" + e.getMessage());
         }
     }
 
@@ -243,7 +278,7 @@ public class Start{
                 if (total_size > 0) {
                     int current_progress = (int) ((downloaded_size * 100) / total_size);
                     if (current_progress > last_progress) {
-                        System.out.print("\rDownloading... " + current_progress + "%");
+                        System.out.print("\r[MCS-SETUP]: Downloading... " + current_progress + "%");
                         last_progress = current_progress;
                     }
                 }
@@ -260,9 +295,9 @@ public class Start{
                 new ProcessBuilder("clear").inheritIO().start().waitFor();
             }
         } catch (java.io.IOException e) {
-            System.out.println("An IO error occurred while trying to clear the console.");
+            System.out.println("[MCS-SETUP]: An IO error occurred while trying to clear the console.");
         } catch (InterruptedException e) {
-            System.out.println("The console clearing process was interrupted.");
+            System.out.println("[MCS-SETUP]: The console clearing process was interrupted.");
             Thread.currentThread().interrupt();
         }
     }
